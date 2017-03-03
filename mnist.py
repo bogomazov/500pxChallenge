96b8bc86b51a88df2d51b466d765944de8a2d7b1
bogomazov <bogomazov1998@gmail.com>
2017-03-03 11:45:01 -0500

    plot graph

--- mnist.py
+++ mnist.py
@@ -1,123 +1,116 @@
+# 500px ML Challenge by Andrey Bogomazov
+
+# What I have learned while conpleting the challenge:
+# 1. Some the front end of TensorFlow.
+# 2. How Tensorflow works (computational graphs, sessions, and some other stuff that is happening in the background)
+# 3. Taking derivatives is very cheap from colah's blog. (regarding backpropagation in neural nets and computational graphs)
+# 4. Reviewed softmax classification.
+
+# Notes:
+# Neural network was not used as it is time consuming to train on my laptop.
+# Results of the training could be saved to avoid retraining every time the code is being run.
+
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.image as mimage
+from matplotlib.backends.backend_pdf import PdfPages
+import logging
 
+# image dimensions
+SAMPLE_DIM = 28
+
+# Tensor Flow Tutorial code
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 sess = tf.InteractiveSession()
 
-x = tf.placeholder(tf.float32, shape=[None, 784])
+x = tf.placeholder(tf.float32, shape=[None, SAMPLE_DIM*SAMPLE_DIM])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
-W = tf.Variable(tf.zeros([784,10]))
+W = tf.Variable(tf.zeros([SAMPLE_DIM*SAMPLE_DIM,10]))
 b = tf.Variable(tf.zeros([10]))
 
 sess.run(tf.global_variables_initializer())
 
 y = tf.matmul(x,W) + b
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
+# With lower regularization it is easier to trick the model
+# as mentioned in the artilcle, hence was not introduced.
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
+SAMPLES_TWO = []
+
+VAL_IMAGE = 0
+VAL_Y = 1
+
+# 1. Feed our model
+# 2. Collect samples of 2
 for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
-    import matplotlib.pyplot as plt
-    image = batch_xs[0]
-    # print batch_xs[0]
-    # print tf.shape(batch_xs[0]).eval()
-    # print tf.size(batch_xs[0]).eval()
-    image = tf.reshape(image, [28, 28])
-    #Not sure you even have to do that if you just want to visualize it
-    #first_array=255*first_array
-    #first_array=first_array.astype("uint8")
-    plt.imshow(image.eval())
-    #Actually displaying the plot if you are not in interactive mode
-    print '------------------'
-    evalled = image.eval()
-    array = image.eval(session=sess)
-    print array
-    import json
-    with open('cache.json', 'wb') as outfile:
-        json.dump(array, outfile)
 
-    plt.savefig("fig.png")
-    plt.show()
-    #Saving plot
+    if len(SAMPLES_TWO) < 10:
+        for j in xrange(len(batch_xs)):
+            sample = (batch_xs[j], batch_ys[j])
+
+            if sample[VAL_Y][2] and len(SAMPLES_TWO) < 10:
+                SAMPLES_TWO += [sample]
     
     train_step.run(feed_dict={x: batch_xs, y_: batch_ys})
 
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+def reshape_image(image_1d):
+    return tf.reshape(image_1d, [SAMPLE_DIM, SAMPLE_DIM])
 
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+def show_image(image_1d):
+    image = reshape_image(image_1d)
+    plt.imshow(image.eval())
 
-print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+    plt.savefig("fig.png")
+    plt.show()
 
-# # mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+def get_adv_image(sample, delta):
+    return (sample, delta, sample + delta)
 
-# # sess = tf.InteractiveSession()
+def classify_image(image):
+    feed_dict = {x: [image]}
+    return tf.argmax(sess.run(y, feed_dict), 1).eval()
 
-# # x = tf.placeholder(tf.float32, shape=[None, 784])
-# # y_ = tf.placeholder(tf.float32, shape=[None, 10])
+def plot_adv_images(samples):
+    rows = len(samples)
+    cols = len(samples[0])
+    gs = gridspec.GridSpec(rows, cols, top=1., bottom=0., right=1., left=0., hspace=0.,
+                wspace=0.)
 
-# # def weight_variable(shape):
-# #   initial = tf.truncated_normal(shape, stddev=0.1)
-# #   return tf.Variable(initial)
+    for i, g in enumerate(gs):
+        ax = plt.subplot(g)
+        image = reshape_image(samples[i/cols][i%cols]).eval()
+        ax.imshow(image)
+        ax.set_xticks([])
+        ax.set_yticks([])
+            
+    plt.savefig("adv_result.png")
+    plt.show()
 
-# # def bias_variable(shape):
-# #   initial = tf.constant(0.1, shape=shape)
-# #   return tf.Variable(initial)
+# Using weights of "six" class as our delta 
+w_vals = sess.run(W)
+w_six = [row[6] for row in w_vals]
+adv_images = [get_adv_image(sample[VAL_IMAGE], w_six) for sample in SAMPLES_TWO]
 
-# # def conv2d(x, W):
-# #   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+# Log classification results 
+# (Some adversarial images might not "trick" the model)
+classified = [classify_image(adv_image[2]) for adv_image in adv_images]
+logging.info('classified as {}'.format(classified))
+plot_adv_images(adv_images)
 
-# # def max_pool_2x2(x):
-# #   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-# #                         strides=[1, 2, 2, 1], padding='SAME')
 
-# # W_conv1 = weight_variable([5, 5, 1, 32])
-# # b_conv1 = bias_variable([32])
-
-# # x_image = tf.reshape(x, [-1,28,28,1])
-
-# # h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-# # h_pool1 = max_pool_2x2(h_conv1)
-
-# # W_conv2 = weight_variable([5, 5, 32, 64])
-# # b_conv2 = bias_variable([64])
-
-# # h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-# # h_pool2 = max_pool_2x2(h_conv2)
-
-# # # Fully connected layer
-# # W_fc1 = weight_variable([7 * 7 * 64, 1024])
-# # b_fc1 = bias_variable([1024])
-
-# # h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-# # h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-# # keep_prob = tf.placeholder(tf.float32)
-# # h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-# # W_fc2 = weight_variable([1024, 10])
-# # b_fc2 = bias_variable([10])
-
-# # y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-
-# # cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=y_))
-# # train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-# # correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-# # accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-# # sess.run(tf.initialize_all_variables())
-# # for i in range(20000):
-# #   batch = mnist.train.next_batch(50)
-# #   if i%100 == 0:
-# #     train_accuracy = accuracy.eval(feed_dict={
-# #         x:batch[0], y_: batch[1], keep_prob: 1.0})
-# #     print("step %d, training accuracy %g"%(i, train_accuracy))
-# #   train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-# # print("test accuracy %g"%accuracy.eval(feed_dict={
-# #     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+# correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 
 
